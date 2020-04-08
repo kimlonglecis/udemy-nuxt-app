292257011f289f59ea62c235bb0252c15bfcbcec
long le <kimlonglecis@gmail.com>
2020-04-08 00:28:44 -0700

    minor changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        udemy-nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        Hello World
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <nuxt-link to="/users">Users</nuxt-link>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+/* import Logo from '~/components/Logo.vue' */
 
 export default {
-  components: {
+  /* components: {
     Logo
-  }
+  } */
 }
 </script>
 
