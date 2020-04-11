a0ef68541f7c2274d4ff944f910a57f692dbb9cf
long le <kimlonglecis@gmail.com>
2020-04-11 13:27:28 -0700

    minor changes

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>single user: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    /* eslint-disable */
+    return data.params.id == 1
+  }
+}
+</script>
